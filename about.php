14ec62688f8274bf37c858cd3553a3b4cbae8f38
badmus <badmus.segun@ymail.com>
2022-04-08 23:55:15 +0200

    update

---
+++ about.php
@@ -0,0 +1,16 @@
+<div class="col-12">
+    <div class="row my-5 ">
+
+        <div class="col-md-12">
+            <div class="card rounded-0 card-outline card-navy shadow" >
+                <div class="card-body rounded-0">
+                    <h2 class="text-center">About</h2>
+                    <center><hr class="bg-navy border-navy w-25 border-2"></center>
+                    <div>
+                        <?= file_get_contents("about_us.html") ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ initialize.php
@@ -0,0 +1,10 @@
+<?php
+$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','http://badmus306.byethost22.com/odjms/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+if(!defined('dev_data')) define('dev_data',$dev_data);
+if(!defined('DB_SERVER')) define('DB_SERVER',"sql102.byethost22.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"b22_31457293");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Hawala306");
+if(!defined('DB_NAME')) define('DB_NAME',"b22_31457293_odjms_db");
+?>
